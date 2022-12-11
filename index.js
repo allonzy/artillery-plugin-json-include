869b9d3123212128049ab4ae58652c421c2d5c80
Simon VIVIER <simon.vivier@skeepers.io>
2022-12-11 16:53:32 +0100

    fix (Issues #1): make the script load at root

--- index.js
+++ index.js
@@ -6,33 +6,17 @@ const fs = require("fs");
 
 function ArtilleryLambdaPlugin(script, events, context) {
   // Prints out "🧑 New load phase started".
-  events.on('phaseStarted', function() {
-    for(let [sc_id, scenario] of Object.entries(script.scenarios)){
-      for(let [fl_id, flow] of Object.entries(scenario.flow)){
-        for(let [ac_id, action] of Object.entries(flow)){
-          if(typeof action.json === 'string'){
-            const match = action.json.match(/^!include\(([^)]+)\)$/);
-            if(match){
-              const baseDir = path.dirname(context.scriptPath);
-              script.scenarios[sc_id].flow[fl_id][ac_id].json  = JSON.parse(fs.readFileSync(path.join(baseDir, match[1])).toString());
-            }
+  for(let [sc_id, scenario] of Object.entries(script.scenarios)){
+    for(let [fl_id, flow] of Object.entries(scenario.flow)){
+      for(let [ac_id, action] of Object.entries(flow)){
+        if(typeof action.json === 'string'){
+          const match = action.json.match(/^!include\(([^)]+)\)$/);
+          if(match){
+            const baseDir = path.dirname(context.scriptPath);
+            script.scenarios[sc_id].flow[fl_id][ac_id].json  = JSON.parse(fs.readFileSync(path.join(baseDir, match[1])).toString());
           }
         }
       }
     }
-  });/*
-  // Prints out "✅ Load phase complete".
-  events.on('phaseCompleted', function() {
-  });
-
-  // Prints out "🖥️ Reporting".
-  events.on('stats', function() {
-
-  });
-
-  // Prints out "🪄 All VUs finished".
-  events.on('done', function() {
-  });
-*/
-  return this;
+  }
 }
